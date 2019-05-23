b11417ca336e6884e2b0fb80647238480cc05f96
Oleh Matviiv <oleh.matviiv@gmail.com>
2019-05-23 13:42:25 +0300

    master - clone.sh

---
+++ clone.sh
@@ -0,0 +1,12 @@
+echo cloning new react project: $1
+echo ==copy src folder
+cp -rf ./src $1/
+echo ==copy .babelrc
+cp ./.babelrc $1/
+echo ==copy .gitignore
+cp ./.gitignore $1/
+echo ==copy webpack.config.js
+cp ./webpack.config.js $1/
+echo ==copy package.json
+cp ./package.json $1/
+echo "==DON'T forget to update package.json with new project related data"
