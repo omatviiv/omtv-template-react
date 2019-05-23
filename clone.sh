b489b8790fe8f0e0ec42a19a20156c3b9e7a03f0
Oleh Matviiv <oleh.matviiv@gmail.com>
2019-05-23 16:07:49 +0300

    master - clone.js - jestsetup.js

--- clone.sh
+++ clone.sh
@@ -7,6 +7,8 @@ echo ==copy .gitignore
 cp ./.gitignore $1/
 echo ==copy webpack.config.js
 cp ./webpack.config.js $1/
+echo ==copy jestsetup.js
+cp ./jestsetup.js $1/
 echo ==copy package.json
 cp ./package.json $1/
 echo "==DON'T forget to update package.json with new project related data"
