8467b92b74bf121f973872e59417a330e94f40ab
omatviiv <oleh.matviiv@gmail.com>
2020-10-09 22:04:32 +0300

    master - update packages

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,5 @@
 // @flow
 import React from 'react';
-import PropTypes from 'prop-types';
-import Input from 'omtv-react-input';
 
 import Component1 from '../component1';
 
@@ -9,8 +7,8 @@ const App = () => (
   <div>
     An empty react app<br/>
     webpack + babel
-    <Input />
     <Component1 />
   </div>
 );
+
 export default App;
